9186795d3f3d9371ef881cbd1eca05046d420be0
mozisheng <809121829@qq.com>
2023-12-19 10:58:46 +0800

    feat: 打包相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    minify: "terser", // 必须启用：terserOptions配置才会有效
+    terserOptions: {
+      compress: {
+        // 生产环境时移除 console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
